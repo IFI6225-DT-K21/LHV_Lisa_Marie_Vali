b5c419d923059530e1db7aadc71b9bdbdb1e5dad
lisamarievali <lisam@tlu.ee>
2021-03-05 20:45:28 +0200

    All elements on page added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Carousel from 'react-bootstrap/Carousel';
 import { MdClose } from "react-icons/md"
 import { FiMenu } from "react-icons/fi"
+import { HiOutlineArrowNarrowRight } from "react-icons/hi"
+import { AiFillPlayCircle } from "react-icons/ai"
+import { AiOutlineInstagram } from "react-icons/ai"
+import { FiFacebook } from "react-icons/fi"
+import { AiOutlineYoutube } from "react-icons/ai"
+import { RiSoundcloudLine } from "react-icons/ri"
+
+
 
 function Navbar() {
   
@@ -14,7 +22,7 @@ function Navbar() {
   }
 
   return (
-    <>
+    <body>
     <nav className='navbar'>
       <div className='contain-nav'>
         <div className='logo'>
@@ -22,17 +30,17 @@ function Navbar() {
         </div>
         <div className='options1'>
     
-          <li className="current"><a href="/">PANK</a></li>
-          <li><a href="/">FINANTSPORTAAL</a></li>
-          <li><a href="/">INVESTOR</a></li>
+          <li className="current">PANK</li>
+          <li>FINANTSPORTAAL</li>
+          <li>INVESTOR</li>
           
           
         </div>
         <div className='options2'>
-          <li><a href="/">AVA KONTO</a><br/></li>
-          <li className="current"><a href="/">SISENE</a>  <br/></li>
-          <li><a href="/">ET</a><br/></li>
-          <li><a href="/">RU</a><br/></li>
+          <li>AVA KONTO<br/></li>
+          <li className="current">SISENE<br/></li>
+          <li>EN<br/></li>
+          <li>RU<br/></li>
         </div>
 
       </div>
@@ -41,9 +49,9 @@ function Navbar() {
     <div className="log-in">
       <div className="log-in-options">
         <li>ID-kaart</li>
-        <li>Mobiil-ID</li>
+        <li className="highlight">Mobiil-ID</li>
         <li>Smart-ID</li>
-        <li>Pin-kalkulaator</li>
+        <li>PIN-kalkulaator</li>
         <li>Salasõna</li>
       </div>
       <div className="input">  
@@ -108,10 +116,109 @@ function Navbar() {
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
+    <div className="text-columns">
+      <div className="text-columns12">
+        <p className="heading">LHV töötajad pühade ajal</p>
+        <p>Seoses Eesti Vabariigi aastapäevaga on ka LHV tööaegades mõningasi muudatusi, 
+          millega palume klientidel rahaasjade planeerimisel arvestada.</p>
+        <p className="lisa">Loe lisa <HiOutlineArrowNarrowRight/></p>
+      </div>
+      <div className="text-columns12">
+      <p className="heading">LHV langetas indeksfondide valitsemistasu poole võrra</p>
+        <p>Alates tänasest langetas LHV passiivselt juhitud pensionifondide valitsemistasud 
+          0,39% pealt 0,20% peale. Selle sammuga muutis LHV oma indeksfondid Eesti soodsaima valitsemistasumääraga pensionifondideks.</p>
+          <p className="lisa">Loe lisa <HiOutlineArrowNarrowRight/></p>
+      </div>
+      <div className="text-columns3">
+      <p className="heading">Kuula LHV Podcasti</p>
+        <p>
+          <li><AiFillPlayCircle className="icons"/>  Hetkeolukord Balti ja USA turgudel</li>
+          <li><AiFillPlayCircle className="icons"/> Urmo Kübar ja Karin Maasel heate...</li>
+          <li><AiFillPlayCircle className="icons"/> Gamestop ja pöörane nädal turgudel</li>
+          <li><AiFillPlayCircle className="icons"/> Mis toimub USA ja Balti turgudel?</li>
+          <li><AiFillPlayCircle className="icons"/> Kristo Aab ja Ülo Kaasik majandu...</li>
+          <li><AiFillPlayCircle className="icons"/> Mihkel Mardna tervishoiust</li>
+          <li><AiFillPlayCircle className="icons"/> Almondi Esco Imepilt AS-i aktsia...</li>
+          <li><AiFillPlayCircle className="icons"/> Martti Siimanniga restoraniärist</li>
+          <li><AiFillPlayCircle className="icons"/> Nelli Janson ja Robin Särg innu...</li>
+          <p className="lisa">Kuula kõiki <HiOutlineArrowNarrowRight/></p>
+        </p>
+      </div>
+
+    </div>
+    <footer className="footer">
+      <div className="footermenu">
+        <div className="footcol1">
+          <li className="foothighlight">TULE KLIENDIKS</li>
+          <li>Eraklient</li>
+          <li>Äriklient</li>
+          <li>Noortepank</li>
+          <li>Lapsekonto</li>
+          <li>Au-klient</li>
+          <li>Privaatpangandus</li>
+          <li>Ava konto <HiOutlineArrowNarrowRight/></li>
+        </div>
+        <div className="footcol1">
+          <li className="foothighlight">KASULIKKUS</li>
+          <li>Hinnakiri ja tingimused</li>
+          <li>Ettepanekud ja kaebused</li>
+          <li>Turvalisus</li>
+          <li>Teata pettusest</li>
+          <li>KKK</li>
 
 
-      
-    </>
+        </div>
+        <div className="footcol1">
+          <li className="foothighlight">LHV</li>
+          <li>Ettevõttest</li>
+          <li>Tööpakkumised</li>
+          <li>Uudised</li>
+          <li>Investor</li>
+          <li>Finantsportaal</li>
+          <li>Roheline LHV</li>
+          <li>Hea tegu</li>
+          <li>Blogi</li>
+        </div>
+        <div className="footcol1">
+        <li></li>
+          <li className="foothighlight">KONTAKT</li>
+          <li>Kontaktid</li>
+          <li>Kontorid</li>
+          <li>Broneeri kohtumisaeg</li>
+          <li>Sularahaautomaadid</li>
+        </div>
+
+      </div>
+
+      <div className="socialicons">
+        <div className="soc1">
+          <AiOutlineInstagram className="socicons"/>
+          <FiFacebook className="socicons"/>
+          <AiOutlineYoutube className="socicons"/>
+          <RiSoundcloudLine className="socicons"/>
+        </div>
+        <div className="soc2">
+          <img className="iconpic" src="pics/app-store-et.svg" alt=""/>
+          <img className="iconpic" src="pics/google-play-et.svg" alt=""/>
+          <img className="iconpic" src="pics/huawei-et.svg" alt=""/>
+        </div>
+          
+
+      </div>
+      <div className="bottomtext">
+        <p>AS LHV Group, Tartu mnt 2, 10145, Tallinn. Oled finantsteenuseid pakkuvate 
+          ettevõtete AS LHV Pank, AS LHV Finance ja AS LHV Varahaldus veebilehel. 
+          Ettevõtetel AS LHV Pank, AS LHV Finance ja AS LHV Varahaldus on õigus salvestada 
+          kõiki neile sidevahendite teel antud korraldusi ja muud suhtlust ning kasutada 
+          vastavaid salvestisi antud korralduste või muude toimingute tõendamiseks. 
+          Enne finantsteenuse lepingu sõlmimist tutvu <u>teenuse tingimustega</u> või küsi lisainfot. 
+          Veebilehte ja sellel sisalduvat informatsiooni kasutades nõustud eeltoodud tingimuste 
+          ja veebilehe <u>kasutustingimustega</u>. <u>Noteeringud on viivitusega</u>.</p>
+      </div>
+
+    </footer>
+    
+  </body>
   );
 }
 
